b88cb5124f52cec1e1b65a0d0476ac0f51465690
rohan331 <78595711+rohan331@users.noreply.github.com>
2025-01-11 17:16:07 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,55 @@
+const apiKey = "9d565e865da85cff0744816866e6b78b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    // if the city is not found, it will show an error message
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        var data = await response.json();
+        // console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "clouds.png";
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "clear.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "rain.png";
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "snow.png";
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "mist.png";
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "drizzle.png";
+        }
+        else if (data.weather[0].main == "Wind") {
+            weatherIcon.src = "wind.png";
+        }
+        else {
+            weatherIcon.src = "humidity.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
